965902a1e9082c58008e1aba5cd7b489cb546993
Kartavya Patel <kevalp566@gmail.com>
2025-07-12 12:17:10 +0530

    added all the files

--- CI_CD_Project1/app.js
+++ CI_CD_Project1/app.js
@@ -1,1 +1,11 @@
-console.log("Hello from Jenkins CI/CD!");
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('<h1>Hello from Jenkins CI/CD! 🚀</h1>');
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`App running on http://localhost:${PORT}`);
+});
